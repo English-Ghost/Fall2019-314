ec920dbbd15ffdac45f50c846e85b644a17d6267
English-Ghost <56802968+English-Ghost@users.noreply.github.com>
2019-11-14 20:37:34 -0600

    twin primes works
    twin primes works and now moved onto the hexagon crosses

--- projectOne/src/Primes.java
+++ projectOne/src/Primes.java
@@ -10,11 +10,19 @@ public class Primes {
 	private class Pair<T> {
 		T firstPair;
 		T secondPair;
+		
+		
+		@Override
+		public String toString() {
+			return firstPair + "," + secondPair;
+		}
+		
+		
 	}
 	
 	// Member variables for containing out lists of integers, twin primes, hexagon crosses, and the pairs of twin primes that make up the hex crosses.
 	ArrayList<BigInteger> primeList = new ArrayList<BigInteger>();
-	ArrayList<Pair> pairList = new ArrayList<Pair>();
+	ArrayList<Pair<BigInteger>> pairList = new ArrayList<>();
 	
 	// Add a prime to the prime list if and only iff it is not already in the list. (ignore duplicates)
 	public void addPrime(BigInteger x)
@@ -40,9 +48,12 @@ public class Primes {
 	// Output the twin prime list. Each twin prime should be on a separate line with a comma separating them, and the total number of twin primes should be on the following line.
 	public void printTwins()
 	{
-		for(int i = 0; i<pairList.size(); i++) {
-			System.out.println("(")
+		for(int i = 0; i<pairList.size(); i++) 
+		{
+			System.out.println(pairList.get(i));
 		}
+		
+		System.out.println("Total Twins: " + pairList.size());
 	}
 		
 	// Output the hexagon cross list. Each should be on a separate line listing the two twin primes and the corresponding hexagon cross, with the total number on the following line.
@@ -54,21 +65,13 @@ public class Primes {
 	public void generatePrimes(int count)
 	{
 		
+		primeList.clear();
+		
 		BigInteger newPrime = new BigInteger("0");
 		int i = 0;
 		
-		/*
-		 * BigInteger addOne = new BigInteger("1"); BigInteger divideTwo = new
-		 * BigInteger("2");
-		 */
-		
 		while(i<count) 
 		{ 
-			/*
-			 * for(BigInteger y = new BigInteger("2"); y.compareTo(y.divide(divideTwo))<=0;y
-			 * = y.add(addOne)) { if(y.mod(y).equals(BigInteger.ZERO)) { primeList.add(y); }
-			 * }
-			 */
 			
 			if(newPrime.compareTo(BigInteger.ONE) == 1) 
 			{
@@ -92,10 +95,32 @@ public class Primes {
 	// Generate and store a list of twin primes.
 	public void generateTwinPrimes()
 	{
+		
+		for(int i = 1; i<primeList.size(); i++)
+		{
+			
+			BigInteger first = primeList.get(i);
+			BigInteger second = primeList.get(i-1);
+			BigInteger diff = (first).subtract(second);
+			BigInteger bTwo = new BigInteger("2");
+			
+			
+			if( diff.compareTo(bTwo) == 0)
+			{
+				Pair<BigInteger> newPair = new Pair<BigInteger>();
+				
+				newPair.firstPair = primeList.get(i-1);
+				newPair.secondPair = primeList.get(i);
+				
+				pairList.add(newPair);
+			}
+			
+		}		
 	}
 	
 	// Generate and store the hexagon crosses, along with the two twin primes that generate the hexagon cross.
 	public void generateHexPrimes()
 	{
+		
 	}
 }
